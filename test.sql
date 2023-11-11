667351324b5f85099017b7be44562d464bd2a3ce
sjmama <ykh45sko@gmail.com>
2023-11-11 17:16:57 +0900

    a

--- test.sql
+++
@@ -1,9 +0,0 @@
--- file name : test.sql
--- pwd : /project_name/app/schema/test.sql
- 
-USE db_1;
- 
-CREATE TABLE testTable (
-    idx      INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    test     VARCHAR(256) NOT NULL
-) CHARSET=utf8;
